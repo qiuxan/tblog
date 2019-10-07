abb41b979acbe6d6dbbc6232382586edbb51b905
qiuxan <qiuxan@gmail.com>
2019-10-07 11:31:03 +0000

    login modify

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 
 
 
 
 Route::group(['prefix'=>'admin','middleware'=>'auth'],function (){
+    Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/post/create',[
         'uses'=>'PostsController@create',
         'as'=>'post.create'
